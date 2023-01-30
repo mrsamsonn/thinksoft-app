d9738f6cb9a000b50c9d791819218b3dac46d3d9
John <98930957+mrsamsonn@users.noreply.github.com>
2023-01-29 23:48:06 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import subprocess
+import string
+import getpass
+
+#flask
+app = Flask(__name__)
+@app.route("/")
+def index():
+    #return "THINKSOFT111"
+    cmd = subprocess.Popen("system_profiler SPHardwareDataType SPStorageDataType SPDisplaysDataType", stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+    cmd_out, cmd_err = cmd.communicate()
+    #filter out attributes
+    for myLine in cmd_out.split('\n'):
+        if "Capacity" in myLine:
+            cap =  myLine.strip()
+        if "Memory" in myLine:
+            mem =  myLine.strip()
+        if "Chip" in myLine:
+            chip =  myLine.strip()
+        if "Serial" in myLine:
+            serial =  myLine.strip()
+        if "Model Identifier" in myLine:
+            model_id =  myLine.strip()
+        if "Resolution" in myLine:
+            resolution = myLine.strip()
+    return cap + '</br>' + mem + '</br>' + chip + '</br>' + serial + '</br>' + model_id + '</br>' + resolution
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
+
+
+#send data from client to server########
+
+
+
+
+exit
